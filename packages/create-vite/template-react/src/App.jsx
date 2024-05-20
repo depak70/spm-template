444bdff1bb9c01f418d4e4945b5e2ac72e26149e
Deepak <deepak@yectra.com>
2024-05-20 15:13:02 +0530

    Modified react-template

--- packages/create-vite/template-react/src/App.jsx
+++ packages/create-vite/template-react/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import SmpLogo from './assets/marketplace-64.png'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={SmpLogo} className="logo" alt="SMP logo" />
+      </a>
+    </div>
+    <h1>SMP</h1>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
       </p>
-    </>
+    <p className="read-the-docs">
+      Click on the SMP logo  to visit the marketplace
+    </p>
+  </>
   )
 }
 
